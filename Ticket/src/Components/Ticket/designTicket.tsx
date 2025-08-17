aad0e48eeac2fd4ac25f6a23a9accd7fe0e38c6e
Mohadese <mohadesehsadr788@gmail.com>
2025-08-17 08:40:19 +0330

    Install react query

--- Ticket/src/Components/Ticket/designTicket.tsx
+++ Ticket/src/Components/Ticket/designTicket.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Background from "../Background";
 import PatternLogo from "../PatternLogo";
 import PatternTop from "../PatternTop";
--- Ticket/src/main.tsx
+++ Ticket/src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 import router from './routing/routes'
 import { RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
