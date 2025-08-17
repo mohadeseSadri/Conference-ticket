ef43ea95128cf525abadcce9203718d09a07de15
Mohadese <mohadesehsadr788@gmail.com>
2025-08-17 08:18:47 +0330

    Save user information in localhost

--- Ticket/src/Components/Form/Form.tsx
+++ Ticket/src/Components/Form/Form.tsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Form() {
-  const [email, ] = useState("");
+  const [email] = useState("");
   const [error, setError] = useState("");
   const validateEmail = (input: string) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(input);
   };
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    username: ""
+  });
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const ticketData = JSON.stringify(formData);
+    localStorage.setItem('ticketData', ticketData);
+    navigate('../Ticket/designTicket')
 
     if (!email) {
       setError("Please enter your email");
@@ -31,33 +42,37 @@ function Form() {
         <div className="flex items-center justify-center">
           <div>
             <div>
-              <p className=" mt-3 mb-2">Full Name</p>
+              <p className="mb-2 mt-3">Full Name</p>
               <div className="flex items-center justify-center">
                 <input
                   type="text"
-                  className="w-[450px] bg-transparent border-2 border-gray-500 border-raduce rounded-lg px-2 py-1"
+                  className="border-raduce w-[450px] rounded-lg border-2 border-gray-500 bg-transparent px-2 py-1"
                 />
               </div>
             </div>
 
             <div>
-              <p className="mt-3 mb-2">Email Address</p>
+              <p className="mb-2 mt-3">Email Address</p>
               <div className="flex items-center justify-center">
                 <input
                   type="email"
-                  className="w-[450px] bg-transparent border-2 border-gray-500 border-raduce rounded-lg px-2 py-1"
+                  className="border-raduce w-[450px] rounded-lg border-2 border-gray-500 bg-transparent px-2 py-1"
                   placeholder="example@email.com"
                 />
               </div>
-              {error && <p className="text-red-500 text-[70%]">Please enter a valid email address</p>}
+              {error && (
+                <p className="text-[70%] text-red-500">
+                  Please enter a valid email address
+                </p>
+              )}
             </div>
 
             <div>
-              <p className=" mt-3 mb-2">GitHub Username</p>
+              <p className="mb-2 mt-3">GitHub Username</p>
               <div className="flex items-center justify-center">
                 <input
                   type="text"
-                  className="w-[450px] bg-transparent border-2 border-gray-500 border-raduce rounded-lg px-2 py-1"
+                  className="border-raduce w-[450px] rounded-lg border-2 border-gray-500 bg-transparent px-2 py-1"
                   placeholder="@yourusername"
                 />
               </div>
@@ -66,7 +81,7 @@ function Form() {
             <div className="flex items-center justify-center">
               <button
                 type="submit"
-                className="w-[450px] bg-[#E97464] mt-3 rounded-lg px-6 py-2 text-black font-bold z-[2]"
+                className="z-[2] mt-3 w-[450px] rounded-lg bg-[#E97464] px-6 py-2 font-bold text-black"
               >
                 <Link to="/ticket">Generate My Ticket</Link>
               </button>
