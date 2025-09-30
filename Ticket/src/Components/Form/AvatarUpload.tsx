b6df403eecaec840d74d24651291cd1c1dfddd11
mohadeseSadri <mohadesehsadr788@gmail.com>
2025-09-30 11:45:55 +0330

    Correcrly setting elements in the avatarUpload component and setting the error message

--- Ticket/src/Components/Form/AvatarUpload.tsx
+++ Ticket/src/Components/Form/AvatarUpload.tsx
@@ -1,5 +1,5 @@
 // AvatarUploader.tsx
-import React, { useState } from "react";
+import { useState } from "react";
 import { Controller, type Control } from "react-hook-form";
 import UploadImg from "../UploadImg";
 
@@ -10,26 +10,15 @@ type Props = {
 
 export function AvatarUpload({ control, name }: Props) {
   const [preview, setPreview] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  const maxSize = 500 * 1024;
 
   const handleFileChange = (
     file: File | null,
     onChange: (file: File | null) => void,
   ) => {
     if (file) {
-      if (file.size > maxSize) {
-        setError("File too large. Please upload a photo under 500KB");
-        onChange(null);
-        setPreview(null);
-        return;
-      }
-      setError("");
       onChange(file);
       setPreview(URL.createObjectURL(file));
     } else {
-      setError("");
       onChange(null);
       setPreview(null);
     }
@@ -42,7 +31,7 @@ export function AvatarUpload({ control, name }: Props) {
         name={name}
         rules={{
           validate: (file: File | null) => {
-            if (!file) return "Click to upload image";
+            if (!file) return "Please upload image";
             if (file.size > 500 * 1024)
               return "File too large. Please upload a photo under 500KB";
             return true;
@@ -50,7 +39,7 @@ export function AvatarUpload({ control, name }: Props) {
         }}
         render={({ field: { onChange }, fieldState: { error } }) => (
           <div className="flex flex-col gap-2">
-            <label className="font-medium text-stone-50">Upload avatar</label>
+            <label className="mt-2 font-medium text-stone-50">Upload avatar</label>
             <div className="flex h-24 w-[450px] flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-400 p-4">
               <input
                 type="file"
@@ -68,86 +57,53 @@ export function AvatarUpload({ control, name }: Props) {
                 }}
               />
 
-              <div className="mt-2 flex">
-                <label
-                  htmlFor="avatarInput"
-                  className="mx-2 cursor-pointer rounded-lg bg-[#403E5A] p-1 text-gray-200 hover:underline"
-                >
-                {preview ? "Change Image" : "Click to upload image"}
-                </label>
-                {preview && (
+              <div className="mt-1 flex-col">
+                {preview ? (
                   <>
-                  <img
-                  src={preview}
-                  alt="Preview"
-                  className="mt-2 h-10 w-10 rounded-lg object-cover"
-                />
-                  <button
-                    type="button"
-                    className="mx-2 rounded-lg bg-[#403E5A] p-1 text-gray-200 hover:underline"
-                    onClick={() => handleFileChange(null, onChange)}
-                  >
-                    Remove Image
-                  </button>
-                  </>)}
-              </div>
+                    <section className="mb-2 flex items-center justify-center">
+                      <img
+                        src={preview}
+                        alt="Preview"
+                        className="flex h-10 w-10 rounded-lg object-cover"
+                      />
+                    </section>
 
-              {error && <p className="text-sm text-red-500">{error.message}</p>}
+                    <section className="flex">
+                      <label
+                        htmlFor="avatarInput"
+                        className="mx-2 cursor-pointer rounded-lg bg-[#403E5A] px-1 text-gray-200 hover:underline"
+                      >
+                        Change Image
+                      </label>
+                      <button
+                        type="button"
+                        className="mx-2 rounded-lg bg-[#403E5A] px-1 text-gray-200 hover:underline"
+                        onClick={() => handleFileChange(null, onChange)}
+                      >
+                        Remove Image
+                      </button>
+                    </section>
+                  </>
+                ) : (
+                  <>
+                    <section className="mb-2 flex items-center justify-center">
+                      <UploadImg />
+                    </section>
+
+                    <label
+                      htmlFor="avatarInput"
+                      className="cursor-pointer rounded-lg bg-[#403E5A] px-1 text-gray-200 hover:underline"
+                    >
+                      Click to upload image
+                    </label>
+                  </>
+                )}
+              </div>
             </div>
+            {error && <p className="text-sm text-[#E97464]">{error.message}</p>}
           </div>
         )}
       />
     </>
-
-    // <Controller
-    //   name={name}
-    //   control={control}
-    //   rules={{
-    //     validate: (file: File | null) => {
-    //     <div className="flex flex-col gap-2">
-    //       <p className="font-medium text-stone-50">Upload avatar</p>
-    //       <div className="flex h-24 w-[450px] flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-400 p-4">
-    //         {preview ? (
-    //           <img
-    //             src={preview}
-    //             alt="Preview"
-    //             className="h-10 w-10 rounded-lg object-cover shadow-md"
-    //           />
-    //         ) : (
-    //           <span className="text-gray-500">
-    //             <UploadImg />
-    //           </span>
-    //         )}
-    //         <input
-    //           type="file"
-    //           accept="image/png, image/jpeg"
-    //           className="hidden"
-    //           id="avatarInput"
-    //           onChange={(e) =>
-    //             handleFileChange(e.target.files?.[0] ?? null, onChange)
-    //           }
-    //         />
-    //         <div className="mt-2 flex">
-    //           <label
-    //             htmlFor="avatarInput"
-    //             className="mx-2 cursor-pointer rounded-lg bg-[#403E5A] p-1 text-gray-200 hover:underline"
-    //           >
-    //             {preview ? "Change Image" : "Click to upload image"}
-    //           </label>
-    //           {preview && (
-    //             <button
-    //               type="button"
-    //               className="mx-2 rounded-lg bg-[#403E5A] p-1 text-gray-200 hover:underline"
-    //               onClick={() => handleFileChange(null, onChange)}
-    //             >
-    //               Remove Image
-    //             </button>
-    //           )}
-    //         </div>
-    //       </div>
-    //       {error && <p className="mb-1 text-sm text-[#F57463]">{error}</p>}
-    //     </div>
-    //   )}
-    // />
   );
 }
